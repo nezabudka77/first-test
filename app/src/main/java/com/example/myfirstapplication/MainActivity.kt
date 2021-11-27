88fddbb0b718305fc227e8bd61241173a39fca39
nezabudka77 <nezabudka77@gmail.com>
2021-11-27 21:31:18 +0300

    second commit

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG="PoemActivity"
+val poem= arrayOf("Мы – пешки, небо же – игрок.",
+"То не мечта моя.",
+"Исполнив всё, что предназначил рок,",
+"На доске бытия,",
+"Мы сходим тихо в темный гроб,",
+"Покой там находя.")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, poem[0])
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, poem[1])
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, poem[2])
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, poem[3])
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, poem[4])
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, poem[5])
     }
 }
